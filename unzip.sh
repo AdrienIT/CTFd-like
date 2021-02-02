f5fb46b555e962ccbe88e094d6be132a05e0b1b2
AdrienIT <adrien.zoghbi@ynov.com>
2021-02-02 12:16:28 +0100

    patch script + add zip file

--- unzip.sh
+++ unzip.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+mkdir /tmp/upload
+
+mkdir /tmp/chall
+
 test="$(ls /tmp/upload | grep .\zip$ |head -1| sed -n 's/.zip//p')"
 
 mkdir /tmp/chall/${test}
 
 unzip /tmp/upload/${test}.zip -d /tmp/chall/${test}
 
+#vagrant init /tmp/chall/${test}/
+
+cd /tmp/chall/${test}/${test}
+vagrant up ${test}
