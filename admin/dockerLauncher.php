e31e673f7283920f0954385a45ed1af25f293087
paul-lautier <paul.lautier@ynov.com>
2021-03-28 19:26:36 +0200

    add remove container

--- admin/dockerLauncher.php
+++ admin/dockerLauncher.php
@@ -47,7 +47,7 @@ if (isset($_GET["name"]) and !empty($_GET["name"])) {
     }
 
 }
-sudo docker rm -f $name
+
 ?>
 
 <!DOCTYPE html>
@@ -112,7 +112,8 @@ sudo docker rm -f $name
                     <button><a href="dockerLauncher.php?name=<?=$challnametmp;?>&action=start">start</a></button>
                     <button><a href="dockerLauncher.php?name=<?=$challnametmp;?>&action=stop">stop</a></button>
                     <button><a href="dockerLauncher.php?name=<?=$challnametmp;?>&action=restart">restart</a></button>
-                    <button><a href="dockerLauncher.php?name=<?=$challnametmp;?>&action=remove">remove</a></button>
+                    <?php if($challstatus === 'Exited'){ echo("<button><a href=\"dockerLauncher.php?name=$challnametmp&action=remove\">remove</a></button>");}?>
+    
 
 
                     
